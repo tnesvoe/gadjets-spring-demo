47c1b0e24b6959d9642472c43c590f4262994ba1
tnesvoe <tnesvoe@gmail.com>
2022-06-30 13:27:28 +0200

    The table does not exist

---
+++ src/main/java/ru/company/gadjets/GadjetsApplication.java
@@ -0,0 +1,13 @@
+package ru.company.gadjets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GadjetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GadjetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/company/gadjets/GadjetsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.company.gadjets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GadjetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
