75953915d9a1e7448a4bb7768043e893531722df
tnesvoe <tnesvoe@gmail.com>
2022-07-21 16:33:00 +0200

    ResponseEntity from TestRestTemplate

--- src/test/java/ru/company/devices/DeviceRestApiTests.java
+++ src/test/java/ru/company/devices/DeviceRestApiTests.java
@@ -1,36 +1,43 @@
 package ru.company.devices;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 import ru.company.devices.controller.DeviceController;
-
-import java.net.http.HttpResponse;
 
 import static ru.company.devices.HttpService.serveClient;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.io.IOException;
+
 import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 
 public class DeviceRestApiTests {
 
     @Autowired
-    DeviceController deviceController;
+    private TestRestTemplate restTemplate;
 
     @Test
-    void autowiredWorks() {
-        assertThat(deviceController).isNotNull();
-    }
-
-    @Test
-    void testReturn200() throws java.io.IOException, java.lang.InterruptedException, java.net.URISyntaxException {
-        HttpResponse<String> response = serveClient("http://localhost:8080/");
-        Assertions.assertEquals(response.statusCode(), 200);
-    }
-
-    @Test
-    void testDevices() {
-        HttpUriRequest
+    void testEndpoints() throws ClientProtocolException, IOException {
+        ResponseEntity<String> response = restTemplate.getForEntity("/", String.class);
+        Assertions.assertEquals(response.getStatusCode(), HttpStatus.OK);
+        ResponseEntity<String> devices = restTemplate.getForEntity("/devices", String.class);
+        Assertions.assertEquals(devices.getStatusCode(), HttpStatus.OK);
+        ResponseEntity<String> clients = restTemplate.getForEntity("/clients", String.class);
+        Assertions.assertEquals(clients.getStatusCode(), HttpStatus.OK);
     }
 }
